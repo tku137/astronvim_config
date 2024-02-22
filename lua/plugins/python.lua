2a404897e4046edd5187eb56a607118e79aa299e
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-02-22 12:50:59 +0100

    adapt python.lua to list_insert_unique breaking changes

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -5,38 +5,34 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(
-          opts.ensure_installed,
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
           "python",
-          "toml"
-        )
+          "toml",
+        })
       end
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "pyright",
-        "ruff_lsp"
-      )
+        "ruff_lsp",
+      })
     end,
   },
   {
     "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(
-      opts.ensure_installed,
-      "black"
-    ) end,
+    opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "black",
+    }) end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
-        "python"
-      )
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "python",
+      })
       if not opts.handlers then opts.handlers = {} end
       opts.handlers.python = function() end -- make sure python doesn't get set up by mason-nvim-dap, it's being set up by nvim-dap-python
     end,
