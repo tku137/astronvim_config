6ce05b992b65b4178d311bb859c5a773ca3f5d64
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-02-22 13:20:07 +0100

    set default LSPs

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -23,8 +23,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
+        "prettier",
+        -- lua
         "stylua",
+        "luacheck"
         -- add more arguments for adding more null-ls sources
       })
     end,
