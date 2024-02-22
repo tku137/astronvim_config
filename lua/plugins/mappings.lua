6be22edecd5d6cedbd0c661a879b007bbe4f9cfa
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-02-22 17:45:09 +0100

    re-add open alpha on last buffer close

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -28,6 +28,15 @@ return {
             end,
             desc = "Pick to close",
           },
+          -- open dashboard when no more buffers open
+          ["<Leader>c"] = {
+            function()
+              local bufs = vim.fn.getbufinfo { buflisted = true }
+              require("astrocore.buffer").close(0)
+              if require("astrocore").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+            end,
+            desc = "Close buffer",
+          },
           -- tables with the `name` key will be registered with which-key if it's installed
           -- this is useful for naming menus
           -- ["<Leader>b"] = { name = "Buffers" },
