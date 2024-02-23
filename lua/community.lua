4da39327a5fda70ba7262dda546d0b7dc4ff0183
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-02-23 21:47:01 +0100

    streamline neotest python config

--- lua/community.lua
+++ lua/community.lua
@@ -75,6 +75,9 @@ return {
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.git.diffview-nvim" },
 
+  -- testing
+  { import = "astrocommunity.test.neotest" },
+
   -- search and replace
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
