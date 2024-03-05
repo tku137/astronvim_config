63281db133dd3d27b7401e7833b8f8301ea3de0b
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-03-05 18:45:37 +0100

    astrolsp: let cpp community handle enconding nonesense

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -53,7 +53,7 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
     -- customize how language servers are attached
     handlers = {
